73c2aa4ea93eaace1a697bdca209cdace1187a4c
Sandeep Singh <sandeep.singh@jellyfishtechnologies.com>
2023-10-02 18:13:53 +0530

    model added

---
+++ lib/models/catalog.dart
@@ -0,0 +1,14 @@
+class CatalogItem {
+  late final int id;
+  late final String name;
+  late final String description;
+  late final num price;
+  late final String picture;
+
+  CatalogItem(
+      {required this.id,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.picture});
+}
